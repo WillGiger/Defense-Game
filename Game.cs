1881d2be69a6b96024f0aa6928af81aaecc186e5
WGiger <bgigs37@gmail.com>
2020-08-18 13:59:39 -0500

    invader and path updated

--- Game.cs
+++ Game.cs
@@ -9,7 +9,23 @@ namespace TreehouseDefense
             //debugging tools 
             try 
             {
-                MapLocation mapLocation = new MapLocation(20,20, map);
+                Path path= new Path(
+                new [] {
+                    new MapLocation(0,2, map),
+                    new MapLocation(1,2, map),
+                    new MapLocation(2,2, map),
+                    new MapLocation(3,2, map),
+                    new MapLocation(4,2, map),
+                    new MapLocation(5,2, map),
+                    new MapLocation(6,2, map),
+                    new MapLocation(7,2, map)
+                });
+
+
+
+                
+
+
 
             } 
             catch(OutOfBoundsException ex)
--- Path.cs
+++ Path.cs
@@ -2,6 +2,19 @@ namespace TreehouseDefense
 {
     class Path
     {
-        
+        private MapLocation[] _path;
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+
+        public MapLocation GetLocationAt(int pathStep)
+        {
+            //return null if out of index
+            return (pathStep < _path.Length) ? _path[pathStep] :  null;
+        }
+
+
+
     }
 }
